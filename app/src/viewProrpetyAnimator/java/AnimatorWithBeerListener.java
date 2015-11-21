0bd712c68ebf7627de3877c87deb90257eca3a5e
davirodrigues07 <davirds07@gmail.com>
2015-11-21 13:58:35 -0200

    prepare product flavors

---
+++ app/src/viewProrpetyAnimator/java/AnimatorWithBeerListener.java
@@ -0,0 +1,17 @@
+package br.com.davirodrigues.animationswithbeer;
+
+
+import android.animation.Animator;
+
+public abstract class AnimatorWithBeerListener implements Animator.AnimatorListener {
+
+    @Override
+    public void onAnimationCancel(Animator animation){
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation){
+
+    }
+}
